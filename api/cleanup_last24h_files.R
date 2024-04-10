203634fe37afd5143b4a9d4856836f7977d8ddd7
Pablo <pcontreras@idealista.com>
2024-04-10 20:12:10 +0200

    Add new cleanup las24 files script

---
+++ api/cleanup_last24h_files.R
@@ -0,0 +1,53 @@
+#' ---
+#' title: Meteo app
+#' author: Pablo Contreras
+#' date: 2024-04-09
+#' description: Script that deletes old "_last24h.csv.gz" files
+#' ---
+
+library(lubridate, warn.conflicts = FALSE, quietly = TRUE)
+
+default_stations <- c("3195", "3129", "2462", "C430E", "1208H", "1249X")
+
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) > 0) {
+  stations <- args
+} else {
+  stations <- default_stations
+}
+
+clean_files <- function(station) {
+
+  file_path <- paste0("~/aemet_data/", station)
+  file_list <- list.files(path = file_path, pattern = "*last24h.csv.gz", full.names = TRUE)
+  current_date <- Sys.Date()
+  
+  for (file in file_list) {
+    # Extraer fecha y hora del nombre del fichero
+    file_date <- as.Date(substr(basename(file), 1, 8), format = "%Y%m%d")
+    file_hour <- substr(basename(file), 10, 13)
+    
+    file_age <- as.numeric(current_date - file_date)
+    
+    # Si el fichero tiene más de 30 días, borrarlo
+    if (file_age > 30) {
+      unlink(file)
+    }
+    
+    # Si tiene menos de 30 días pero no es de las 00:00 o 12:00, borrarlo
+    # a menos que sea el fichero más reciente del día actual
+    else if (file_date < current_date & !file_hour %in% c("0000","1200")) {
+      # Obtener el fichero más reciente para este día
+      todays_files <- list.files(path = file_path, pattern = paste0(format(current_date, "%Y%m%d"),"*last24h.csv.gz"), full.names = TRUE)
+      most_recent <- todays_files[which.max(file.info(todays_files)$mtime)]
+      
+      # Borrar el fichero si no es el más reciente
+      if (file != most_recent) {
+        unlink(file)
+      }
+    }
+  }
+}
+
+# Aplicar la función a cada station
+sapply(stations, clean_files)
