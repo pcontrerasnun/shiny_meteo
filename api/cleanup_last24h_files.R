649ca97693345f4df5c64f519214bed006233b6f
Pablo <pcontreras@idealista.com>
2024-04-10 20:54:36 +0200

    Fix script

--- api/cleanup_last24h_files.R
+++ api/cleanup_last24h_files.R
@@ -16,8 +16,6 @@ if (length(args) > 0) {
   stations <- default_stations
 }
 
-print(stations)
-
 clean_files <- function(station) {
 
   file_path <- paste0("~/aemet_data/", station)
@@ -27,7 +25,7 @@ clean_files <- function(station) {
   for (file in file_list) {
     # Extraer fecha y hora del nombre del fichero
     file_date <- as.Date(substr(basename(file), 1, 8), format = "%Y%m%d")
-    file_hour <- substr(basename(file), 10, 13)
+    file_hour <- substr(basename(file), 10, 11)
     
     file_age <- as.numeric(current_date - file_date)
     
@@ -38,9 +36,9 @@ clean_files <- function(station) {
     
     # Si tiene menos de 30 días pero no es de las 00:00 o 12:00, borrarlo
     # a menos que sea el fichero más reciente del día actual
-    else if (file_date < current_date & !file_hour %in% c("0000","1200")) {
+    else if (file_date < current_date & !file_hour %in% c("00","12")) {
       # Obtener el fichero más reciente para este día
-      todays_files <- list.files(path = file_path, pattern = paste0(format(current_date, "%Y%m%d"),"*last24h.csv.gz"), full.names = TRUE)
+      todays_files <- list.files(path = file_path, pattern = paste0("^", format(Sys.Date(), "%Y%m%d"), "_\\d{6}_\\d{4}_last24h\\.csv\\.gz$"), full.names = TRUE)
       most_recent <- todays_files[which.max(file.info(todays_files)$mtime)]
       
       # Borrar el fichero si no es el más reciente
