32d3da20f436517a9807397816524588b54960b3
Pablo Contreras <piblo95@hotmail.com>
2025-01-05 16:26:03 +0100

    tipos...

--- api/aemet_api_hist.R
+++ api/aemet_api_hist.R
@@ -48,9 +48,20 @@ tryCatch({
     
     if(station == "1249X") {
       data1 <- climaemet::aemet_daily_clim(
-        station = station, start = ref_start_date, end = ref_end_date)
+        station = station, start = ref_start_date, end = ref_end_date) |> 
+        dplyr::mutate(indicativo = as.numeric(indicativo)) |> 
+        dplyr::mutate(prec = as.character(prec)) |> 
+        dplyr::mutate(horatmax = as.character(horatmax)) |> 
+        dplyr::mutate(horaHrMax = as.character(horaHrMax)) |> 
+        dplyr::mutate(horaHrMin = as.character(horaHrMin))
+      
       data2 <- climaemet::aemet_daily_clim(
-        station = "1249I", start = ref_start_date, end = ref_end_date)
+        station = "1249I", start = ref_start_date, end = ref_end_date)  |> 
+        dplyr::mutate(indicativo = as.numeric(indicativo)) |> 
+        dplyr::mutate(prec = as.character(prec)) |> 
+        dplyr::mutate(horatmax = as.character(horatmax)) |> 
+        dplyr::mutate(horaHrMax = as.character(horaHrMax)) |> 
+        dplyr::mutate(horaHrMin = as.character(horaHrMin))
       
       historical_data_new <- dplyr::bind_rows(historical_data, data1, data2) |> 
         dplyr::distinct(fecha, .keep_all = TRUE) |> # Remove duplicated rows, keep first
