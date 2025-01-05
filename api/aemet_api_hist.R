b71f945dde95fbb60a537ec5ccfe9f8ac5526d92
Pablo Contreras <piblo95@hotmail.com>
2025-01-05 16:10:56 +0100

    mas tipos...

--- api/aemet_api_hist.R
+++ api/aemet_api_hist.R
@@ -39,7 +39,8 @@ tryCatch({
     files <- list.files(path, pattern = "historical")
     print(paste0("Loading from local storage historical data for station ", station, ": ", tail(files, 1)))
     historical_data <- readr::read_csv(paste0(path, tail(files, 1)), show_col_types = FALSE) |> 
-      dplyr::mutate(horaHrMax = as.character(horaHrMax))
+      dplyr::mutate(horaHrMax = as.character(horaHrMax)) |> 
+      dplyr::mutate(horaHrMin = as.character(horaHrMin))
     
     ref_start_date <- max(historical_data$fecha) # Ultima fecha disponible en el fichero
     ref_end_date <- max(historical_data$fecha) + lubridate::days(182) # Ultima fecha + 6 meses
