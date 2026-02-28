250f75d78b27c256dcc89707f9478f0f866969d9
Pablo Contreras <piblo95@hotmail.com>
2026-02-28 09:49:10 +0100

    update missing dict for station 1059X

--- helpers/debugging.R
+++ helpers/debugging.R
@@ -6,14 +6,14 @@
 #' ALWAYS LEAVE ALL THE CODE COMMENTED OR YOU MAY HAVE ERRORS
 #' ---
 
-#data_clean <- rdrop2::drop_read_csv("/aemet_data/1059X/20251228_180125_1059X_complete.csv.gz", colClasses = c(day = "character", month = "character", date = "Date"))
+#data_clean <- rdrop2::drop_read_csv("/aemet_data/1249X/20260228_060150_1249X_complete.csv.gz", colClasses = c(day = "character", month = "character", date = "Date"))
 #data_clean2 <- rdrop2::drop_read_csv("/aemet_data/C430E/20240408_000108_C430E_complete.csv.gz", colClasses = c(day = "character", month = "character", date = "Date"))
 #data_clean <- FetchAEMETData(station = "3195") # 2462 C430E 3195
 #data_temp <- CleanTempData(data = data_clean[[1]])
 #data_pcp <- CleanPcpData(data = data_clean[[1]])
 #
 #data <- climaemet::aemet_daily_clim(
-#  station = "0200E", start = "1900-01-01", end = Sys.Date(), verbose = TRUE)
+#  station = "1059X", start = Sys.Date()-180, end = Sys.Date(), verbose = TRUE)
 #
 #data <- climaemet::aemet_last_obs(station = "3129", verbose = TRUE)
 #
@@ -28,6 +28,26 @@
 #archivos <- list.files('/home/ubuntu/aemet_data/1249X/', full.names = TRUE)
 #archivos_last24 <- archivos[grep("last24", archivos)]
 #for (archivo in archivos_last24) {
-#data <- read.csv(archivo, header = TRUE)
+#data <- read.csv(archivo, header = TRUE) |> 
 #cat("Archivo:", basename(archivo), "\tNúmero de columnas:", ncol(data), "\n")
 #}
+
+# DESCARGAR ULT FICHERO DISPONIBLE DE DROPBOX PARA TODAS LAS STATIONS 
+# Y MIRAR NUMERO DE MISSINGS
+#stations <- c("3195", "3129", "2462", "C430E", "1208H", "1249X", "1059X")
+#for (station in stations) {
+#  files <- rdrop2::drop_search(paste0(station, "_complete"), max_results = 1000)
+#  
+#  files_metadata <- lapply(files$matches, function(match) match$metadata)
+#  files_df <- do.call(rbind, lapply(files_metadata, as.data.frame, stringsAsFactors = FALSE))
+#  sorted_files <- files_df[order(files_df$server_modified, decreasing = TRUE), ]
+#  last_file_name <- sorted_files[1,]$path_lower
+#  last_file_data <- rdrop2::drop_read_csv(last_file_name, 
+#                                          colClasses = c(day = "character", month = "character", date = "Date"))  
+#  print(paste0("---STATION ", station, " ---"))
+#  print(paste0("PCP: ", sum(is.na(last_file_data$pcp)))) 
+#  print(paste0("TMIN: ", sum(is.na(last_file_data$tmin)))) 
+#  print(paste0("TMAX: ", sum(is.na(last_file_data$tmax)))) 
+#  print(paste0("TMEAN: ", sum(is.na(last_file_data$tmean)))) 
+#}
+
